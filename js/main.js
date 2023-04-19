b5dfa8c42e42b5ad77051b89a2afd3008311b5b8
Alan Schanzenbach <alanschanzenbach@gmail.com>
2023-04-19 06:41:42 -0300

    contáctanos.html agregado con api reqres funcional

--- js/main.js
+++ js/main.js
@@ -31,5 +31,40 @@ $(document).ready(function() {
             placeThree.classList.add("up");
         }
     });
+
+
+
+
+    $('#send-btn').click(function(){
+        let nombre = $('#nombre').val();
+        let apellido = $('#apellido').val();
+        let tel = $('#tel').val();
+        let options = $('#options-input').val();
+        let email = $('#email').val();
+        let contacto = document.querySelector('input[name="contacto"]:checked').value;
+        let mensaje = $('#comments').val();
+        $.ajax({
+            url: 'https://reqres.in/api/users', 
+            method: 'POST', 
+            data: {
+                first_name: nombre,
+                last_name: apellido,
+                email: email,
+                phone_number: tel,
+                options: options,
+                contact: contacto,
+                mensaje: mensaje
+            },
+            success: function(response) {
+                console.log('Éxito:', response);
+                alert('¡Mensaje enviado con éxito!');
+            },
+            error: function(xhr, status, error) {
+                console.error('Error:', error);
+                alert('Error al enviar el mensaje. Por favor inténtelo nuevamente.');
+            }
+        });
+    });
+
 });
 
